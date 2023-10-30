c37a5fc919db6cea51648a4ad985b65f264f8d64
Mperorrc <115109198+Mperorrc@users.noreply.github.com>
2023-10-30 10:50:09 +0530

    Issues - transfer of money

--- discounts.sol
+++ discounts.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+// import "/contracts/erc20token.sol";
+
 contract ProductMarket {
     
     struct Product {
@@ -14,12 +16,16 @@ contract ProductMarket {
         uint256 productId;
     }
 
+    address constant public companyABC = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+
     mapping(uint256 => Product) products;
     mapping(address => Purchase[]) clientPurchases;
+    mapping(address => uint256) clientTokens;
 
     uint256 productCount;
 
-    event ProductPurchased(address buyer, uint256 productId);
+    event EtherTransferred(address indexed from, address indexed to, uint256 amount);
+    event LogMessage(uint256 message);
 
     constructor() {
         productCount = 0;
@@ -31,88 +37,70 @@ contract ProductMarket {
     }
 
     function addProduct(string memory _name, uint256 _price) private {
-        productCount++;
         products[productCount] = Product(productCount, _name, _price);
+        productCount++;
     }
 
-    function buyProduct(uint256 _productId) payable public {
+    function buyProduct(uint256 _productId) public returns(uint256) {
         require(_productId > 0 && _productId <= productCount, "Invalid product ID");
-
-        uint price = products[_productId].price;
-        uint discount = getDiscount(price);
-        price-=discount;
-
-        require(msg.value >= price, "Insufficient funds");
-        clientPurchases[msg.sender].push(Purchase(msg.sender, _productId));
-        emit ProductPurchased(msg.sender, _productId);
+        require(msg.sender!=companyABC,"The Company account is not meant to buy products.");
+        uint256 tokenCount=0;
+        uint256 discountedPrice = getDiscount(products[_productId].price,tokenCount);
+        emit LogMessage(discountedPrice*(10**16));
+        // uint256 accBalance = getBalance();
+        // emit LogMessage(accBalance);
+        // require(accBalance >= discountedPrice, "Insufficient funds");
+        // bool success = _sendEther(discountedPrice*(10**16));
+        // emit LogMessage(discountedPrice);
+        return discountedPrice;    
     }
 
-    function getDiscount(uint price) private view returns(uint discount){
-        if(clientPurchases[msg.sender].length>=50){
-            return price/2;
-            
+
+
+    function _sendEther(uint256 amount) internal returns(bool){
+        require(amount > 0, "Value must be greater than 0");
+
+        (bool success, ) = payable(companyABC).call{value: amount}("");
+        require(success, "Purchase failed");    
+        emit EtherTransferred(address(this), companyABC, amount);
+        return success;
+    }
+
+    function getBalance() private view returns(uint256){
+        return address(this).balance;
+    }
+
+
+    function getDiscount(uint _price,uint256 _tokenCount) private returns(uint discount){
+        if(_tokenCount>=50){
+            _price = _price/2;
+            clientTokens[msg.sender]-=50;
         }
-        if(clientPurchases[msg.sender].length>=20){
-            return price*3/4;
+        if(_tokenCount>=20){
+            _price = _price*3/4;
+            clientTokens[msg.sender]-=20;
         }
-        if(clientPurchases[msg.sender].length>=10){
-            return price*9/10;
+        if(_tokenCount>=10){
+            _price = _price*9/10;
+            clientTokens[msg.sender]-=10;
         }
-        return 0;
+        return _price;
     }
 
     function getClientPurchases(address _clientAddress) public view returns (Purchase[] memory) {
         return clientPurchases[_clientAddress];
     }
 
-    // function getProducts() public view returns (Product[] memory){
-
-    // }
+    function getProducts() public view returns (Product[] memory){
+        Product[] memory displayProducts = new Product[](productCount);
+        for (uint i = 0; i < productCount; i++) {
+            displayProducts[i] = products[i];
+        }
+        return displayProducts;
+    }
 }
 
 
-
-// contract Storage{
-    
-//     struct Client {
-//         address clientAddress;
-//         string name;
-//         uint balance;
-//     }
-
-//     mapping(address => Client) public clients;
-
-//     struct Product{
-//         string name;
-//         uint price;
-//     }
-
-//     mapping(address => Client) public clients;
-
-//     function addClient(string memory _name, uint _initialBalance) public {
-//         require(!isClient(msg.sender), "Address already exists as a client");
-//         Client memory newClient = Client(msg.sender, _name, _initialBalance);
-//         clients.push(newClient);
-//     }
-
-//     function isClient(address _clientAddress) internal view returns (bool) {
-//         for (uint i = 0; i < clients.length; i++) {
-//             if (clients[i].clientAddress == _clientAddress) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-
-//     function getClients() public view returns (Client[] memory){
-//         return clients;
-//     }
-
-//     function getProducts() public view returns (Product[] memory){
-
-//     }
-
-// }
 
 
 
