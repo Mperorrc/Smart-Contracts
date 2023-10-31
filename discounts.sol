9a4db6671053a1d8a8dbffcaa734840ecdf14387
Mperorrc <115109198+Mperorrc@users.noreply.github.com>
2023-10-31 22:01:28 +0530

    Issue - uncomment few lines

--- discounts.sol
+++ discounts.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-// import "/contracts/erc20token.sol";
+import "/contracts/erc20token.sol";
 
 contract ProductMarket {
     
@@ -16,20 +16,24 @@ contract ProductMarket {
         uint256 productId;
     }
 
+    DiscountToken token;
+
     address constant public companyABC = 0xc88050a842054ed74862F5bbEF0a247ac031C057;
 
     mapping(uint256 => Product) products;
-    mapping(address => uint256) clientTokens;
+    mapping(address => uint256) tempWallet;
 
     uint256 productCount;
 
     event EtherTransferred(address indexed from, address indexed to, uint256 amount);
     event LogMessage(uint256 message);
-    event Received(address sender, uint256 amount);
     event SentToCompanyABC(uint256 amount);
     event Refunded(address receiver, uint256 amount);
+    event Failed(string message);
+    event Received(address sender, uint256 amount);
 
-    constructor() {
+    constructor(address _contractAddress) {
+        token = DiscountToken(_contractAddress);
         productCount = 0;
         addProduct("Product 1", 0.01 ether);
         addProduct("Product 2", 0.015 ether);
@@ -42,54 +46,49 @@ contract ProductMarket {
         products[productCount] = Product(productCount, _name, _price);
         productCount++;
     }
-
-    function buyProduct(uint256 _productId) public returns(uint256) {
+    
+    function buyProduct(uint256 _productId,address acc) public{
         require(_productId >= 0 && _productId <= productCount, "Invalid product ID");
-        require(msg.sender!=companyABC,"The Company account is not meant to buy products.");
-        uint256 tokenCount=0;
-        uint256 discountedPrice = getDiscount(products[_productId].price,tokenCount);
-        
+        require(acc!=companyABC,"The Company account is not meant to buy products.");
+        uint256 tokenCount=getTokenBalance(acc);
+        uint256 discountedPrice = getDiscount(products[_productId].price,tokenCount,acc);
         emit LogMessage(discountedPrice);
-        uint256 balance = getBalance();
-        if(discountedPrice<=balance){
-            uint256 excessAmount = balance - discountedPrice;
-            // Transfer the excess amount back to the sender
-            if (excessAmount > 0) {
-                payable(msg.sender).transfer(excessAmount);
-                emit Refunded(msg.sender, excessAmount);
-            }
-            // Transfer the accepted amount to COMPANYABC
-            payable(companyABC).transfer(discountedPrice);
-            emit SentToCompanyABC(discountedPrice);
+        uint256 balance = getBalance(acc);
+        require(discountedPrice<=balance,"Insufficient Funds");
+        payable(companyABC).transfer(discountedPrice);
+        tempWallet[acc]-=discountedPrice;
+        if(getBalance(companyABC)>0){
+            token.mintOne();     
         }
-        else{
-            payable(msg.sender).transfer(balance);
-            emit Refunded(msg.sender, balance);       
-        }
-
-        return discountedPrice;    
+        // token.transferToUser(acc,1*(10**18));
+        emit SentToCompanyABC(discountedPrice);
     }
 
     receive() external payable {
         emit Received(msg.sender, msg.value);        
+        tempWallet[msg.sender]+=msg.value;
     }
 
-    function getBalance() public view returns(uint256){
-        return address(this).balance;
+    function getBalance(address add) public view returns(uint256){
+        return tempWallet[add];
     }
 
-    function getDiscount(uint _price,uint256 _tokenCount) private returns(uint discount){
+    function getTokenBalance(address add) public view returns(uint256){
+        return token.balanceOf(add);
+    }
+
+    function getDiscount(uint _price,uint256 _tokenCount,address acc) private returns(uint discount){
         if(_tokenCount>=50){
             _price = _price/2;
-            clientTokens[msg.sender]-=50;
+            token.transferBetweenAccounts(acc, companyABC, 50*(10**18));
         }
-        if(_tokenCount>=20){
+        else if(_tokenCount>=20){
             _price = _price*3/4;
-            clientTokens[msg.sender]-=20;
+            token.transferBetweenAccounts(acc, companyABC, 20*(10**18));
         }
-        if(_tokenCount>=10){
+        else if(_tokenCount>=10){
             _price = _price*9/10;
-            clientTokens[msg.sender]-=10;
+            token.transferBetweenAccounts(acc, companyABC, 10*(10**18));
         }
         return _price;
     }
@@ -100,6 +99,12 @@ contract ProductMarket {
             displayProducts[i] = products[i];
         }
         return displayProducts;
+    }
+
+    function withdraw(address acc) public{
+        emit Refunded(acc, tempWallet[acc]);
+        payable(acc).transfer(tempWallet[acc]);
+        tempWallet[acc]=0;
     }
 }
 
