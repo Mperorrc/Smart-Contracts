46cfac22d1a532679d419f304b0feb708d71d2af
Mperorrc <115109198+Mperorrc@users.noreply.github.com>
2023-10-31 22:02:46 +0530

    issue - idk

--- erc20token.sol
+++ erc20token.sol
@@ -4,8 +4,22 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract DiscountToken is ERC20 {
+    uint256 tokensLeft = 0;
+
     constructor() ERC20("Discount Tokens", "DTK") {
-        _mint(msg.sender, 1);
+        mintOne();
+    }
+
+    function mintOne() public {
+        _mint(msg.sender, 1*(10**18));
+    }
+
+    function transferToUser(address _to,uint256 value) public {
+        transfer(_to, value);
+    }
+
+    function transferBetweenAccounts(address _from,address _to,uint256 value) public{
+        transferFrom(_from,_to,value);
     }
 
 }
